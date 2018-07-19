7c2245bf54d2e5545c73a877e275eaee3bcbccdf
Rupesh Mangal <rupesh.code@gmail.com>
2018-07-20 03:23:37 +0530

    Added allowed host

--- tutfinder/settings.py
+++ tutfinder/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e&9!tyxvdtil^y)rl95=ixo9j5kfl%gv49v5k64+teok2hcqh9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://tutfinder-api.ap-south-1.elasticbeanstalk.com/']
 
 
 # Application definition
